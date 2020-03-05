916fcec41b8a3aa477fed2edd0bb78f933bd5607
SubZiro <webnlevel@gmail.com>
2020-03-05 23:14:17 +0400

    +links

--- blog/resources/views/errors/404.blade.php
+++ blog/resources/views/errors/404.blade.php
@@ -31,7 +31,7 @@
 			<h1>404</h1>
 			<h2>Oops! Page Not Be Found</h2>
 			<p>Sorry but the page you are looking for does not exist, have been removed. name changed or is temporarily unavailable</p>
-			<a href="#">Back to blog</a>
+			<a href="/">Back to blog</a>
 		</div>
 	</div>
 
